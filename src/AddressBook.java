35c96fe05f0fa0e25ba1473b9542b4ee77d47886
Kshitij Sawhney <59340059+kshitijsaw@users.noreply.github.com>
2020-10-02 19:19:30 +0530

    Initial commit

---
+++ src/AddressBook.java
@@ -0,0 +1,32 @@
+//Kshitij Sawhney Lab 3 (2/10/20)
+
+import java.util.*;
+public class AddressBook {
+    private ArrayList<BuddyInfo> book;
+
+    public AddressBook() {
+        this.book = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if(buddy!=null){
+            this.book.add(buddy);
+        }
+    }
+
+    public BuddyInfo removeBuddy(int index) {
+        if(index>=0 && index < book.size()) {
+            return this.book.remove(index);
+        }
+        else return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        BuddyInfo buddy = new BuddyInfo("Dominic", "Carleton");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(0);
+
+    }
+}
---
+++ src/BuddyInfo.java
@@ -0,0 +1,22 @@
+/*Kshitij Sawhney Lab 1*/
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+
+    public BuddyInfo(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public static void main(String[] args) {
+    }
+}
